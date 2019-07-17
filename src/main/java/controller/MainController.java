361d75f81412e85f197e64ac6cd0c4531fbdf206
lfedyu <lila06121900@gmail.com>
2019-07-18 00:54:09 +0300

    fix var name, dependency

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -5,14 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 @Controller
 public class MainController {
 
     @RequestMapping(value = "/add")
-    public ModelAndView add(@RequestParam("n1") int i, @RequestParam("n2") int j, HttpServletRequest request, HttpServletResponse response){
+    public ModelAndView add(@RequestParam("number1") int i, @RequestParam("number2") int j){
 
         ModelAndView mv = new ModelAndView();
         mv.setViewName("result.jsp");
