264da1437262c3aeb1f38ffdc3235a9f9bb162cb
lfedyu <lila06121900@gmail.com>
2019-08-01 09:43:25 +0300

    added unit test

--- src/main/java/config/ProjConfig.java
+++ src/main/java/config/ProjConfig.java
@@ -3,17 +3,19 @@ package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@EnableWebMvc
 @Configuration
 @ComponentScan({"controller"})
 public class ProjConfig {
 
-    /*@Bean
+    @Bean
     public InternalResourceViewResolver viewResolver(){
         InternalResourceViewResolver vr = new InternalResourceViewResolver();
         vr.setPrefix("/WEB-INF/");
         vr.setSuffix(".jsp");
         return vr;
-    }*/
+    }
 }
