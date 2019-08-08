713f962a6808844863c8d2765d2980602e044f06
Liliya Fedyushko <liliya.fedyushko@visiquate.com>
2019-08-08 20:45:43 +0300

    uncomment

--- src/test/java/controller/GreetControllerIntegrationTest.java
+++ src/test/java/controller/GreetControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package controller;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -7,8 +8,13 @@ import org.springframework.test.context.web.WebAppConfiguration;
 
 import config.ProjConfig;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { ProjConfig.class })
 @WebAppConfiguration
 public class GreetControllerIntegrationTest {
+
+    @Test
+    public void integrationTest(){
+
+    }
 }
